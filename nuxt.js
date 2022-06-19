fcf8897787028c5967f04d4c434ede6df17ecdfd
Khalid Shah <khalid141712@gmail.com>
2022-06-19 16:08:39 +0300

    adding button

---
+++ nuxt.js
@@ -0,0 +1,14 @@
+import {join} from "pathe";
+import {defineNuxtModule} from "@nuxt/kit";
+
+
+export default defineNuxtModule({
+    hooks:{
+        "components:dirs"(dirs){
+            dirs.push({
+                path:join(__dirname, "components"),
+                prefix:"nitx",
+            });
+        },
+    },
+});
