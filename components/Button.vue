b11266ffbe8873c6dab423a46a63a823f8aa8629
Khalid Shah <khalid141712@gmail.com>
2022-06-19 18:31:08 +0300

    adding tailwind

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="inline-block px-12 py-3 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring" href="/download">
-  <slot/>
-    </a>
+    <button class="inline-block px-12 py-3 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded active:text-indigo-500 hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring" href="/download">
+    <slot/>
+    </button>
 </template>
