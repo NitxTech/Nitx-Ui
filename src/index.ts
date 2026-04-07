109bc5e195d087a69f1b6d719c46e986f004ef58
HamzaDev <hmzt25235@gmail.com>
2026-04-07 15:46:11 +0200

    add new components to export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./components/product-switcher/product-switcher";
 export * from "./components/user-account/account";
 export * from "./components/space-selector";
+export * from "./components/spaces";
+export * from "./components/spaces";
